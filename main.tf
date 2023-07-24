047a43eddd3feabc31fb8b7329ed30c828c6e861
Carlos Balcazar <cbalcazarp@gmail.com>
2023-07-24 14:18:23 -0500

    comment backend block

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "tranzact_RG"
-    storage_account_name = "tranzactcbalcazar4585"
-    container_name       = "tranzactcbalcazar4585container"
-    key                  = "terraform.tfstate"
-  }
-}  
+#terraform {
+#  backend "azurerm" {
+#    resource_group_name  = "tranzact_RG"
+#    storage_account_name = "tranzactcbalcazar4585"
+#    container_name       = "tranzactcbalcazar4585container"
+#    key                  = "terraform.tfstate"
+#  }
+#}  
 
 module "networking_module" {
   source                        = "./networking"
